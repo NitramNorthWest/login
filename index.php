12c09e8e0e2e83d02b9eae7d4b78ce8d0ad6b5ac
NitramNortWest <nitramdiseno@gmail.com>
2016-02-14 11:26:53 -0600

    Inital Commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <meta charset="utf-8">
+   <title>Kyle Plancich | Web Developer</title>
+   <link rel="stylesheet" href="css/normalize.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat|Source+Sans+Pro:400,400italic,700italic,700|Ubuntu:400,700|Merriweather:400,400italic|Poiret+One|Pacifico' rel='stylesheet' type='text/css'>
+   <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <meta name=" viewport" content=" width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <header>
+      <a href="index.html" id="logo">
+        <h1>Kyle Plancich</h1>
+        <h2>Designer | Developer</h2>
+      </a>
+      <nav>
+        <ul>
+          <li><a href="login.html">Login</a></li>
+          <li><a href="index.html" class="selected">Portfolio</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div id="wrapper">
+      <section>
+        <ul id="gallery">
+          <li>
+              <a href="img/seattle_public_market_coffee.jpg">
+                <img src="img/seattle_public_market_coffee.jpg" alt="">
+                <p>My favorite place to drink coffee.</p>
+               </a>
+            </li>
+            <li>
+              <a href="img/seattle_moon.jpg">
+                <img src="img/seattle_moon.jpg" alt="">
+                <p>Amaizing sunset.</p>
+               </a>
+            </li>
+            <li>
+              <a href="img/space_needle.jpg">
+                <img src="img/space_needle.jpg" alt="">
+                <p>Best place in the world to code.</p>
+               </a>
+            </li>
+            <li>
+              <a href="img/seattle_hammer_man.jpg">
+                <img src="img/seattle_hammer_man.jpg" alt="">
+                <p>Working hard.</p>
+               </a>
+            </li>
+            <li>
+              <a href="img/seattle_back_side.jpg">
+                <img src="img/seattle_back_side.jpg" alt="">
+                <p>Best backside of any city.</p>
+               </a>
+            </li>
+            <li>
+              <a href="img/pike_place_market.jpg">
+                <img src="img/pike_place_market.jpg" alt="">
+                <p>Awesome market.</p>
+               </a>
+            </li>
+        </ul>
+      </section>
+      <footer>
+        <a href="https://twitter.com/kyleandsuzy">
+        <img src="img/twitter-wrap.png" alt="Twitter Logo" class=" social-icon"></a>
+  
+        <a href="https://www.facebook.com/kyle.plancich.7">
+        <img src="img/facebook-wrap.png" alt="FaceBook Logo" class=" social-icon"></a>
+        <p>&copy; 2016 Kyle Plancich | Nitram Design.</p>
+      </footer> 
+    </div>
+  </body>
+</html>  
+
+
---
+++ login.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>Nitram Diseno</title>
+   <link rel="stylesheet" href="css/normalize.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat|Source+Sans+Pro:400,400italic,700italic,700|Ubuntu:400,700|Merriweather:400,400italic|Poiret+One|Pacifico' rel='stylesheet' type='text/css'>
+   <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <meta name=" viewport" content=" width=device-width, initial-scale=1.0">
+  </head>
+<body>
+<h1>Login to Nitam Diseno</h1>
+      <form method="post" action="signup.php">
+        <p><input type="text" name="login" value="" placeholder="Username or Email"></p>
+        <p><input type="password" name="password" value="" placeholder="Password"></p>
+        <p class="remember_me">
+          <label>
+            <input type="checkbox" name="remember_me" id="remember_me">
+            Remember me on this computer
+          </label>
+        </p>
+        <p class="submit"><input type="submit" name="commit" value="Login"></p>
+      </form>
+     	<nav>
+        <ul>
+          <li><a href="login.html" class="selected">Login</a></li>
+          <li><a href="index.html">Portfolio</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+
+
+
+
+
+
+
+      <section id="seattle_background">
+
+     </section>
+
+
+    
+
+
+
+
+      <footer>
+      	<p>We cannot solve our problems with the same thinking we used to creat them. -Albert Einstein</p>
+        <p>&copy; 2016 Kyle Plancich | Nitram Design.</p>
+      </footer> 
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
